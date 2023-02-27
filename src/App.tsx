b3250d5d50e3acb366af585981780aa26a6203f6
Danylo Krutoholov <danylo_krutoholov@epam.com>
2023-02-27 23:54:51 +0300

    feat(task-1): setup initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Square} from './components/index';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Square />
+    </SafeAreaView>
+  );
+}
+
+export default App;
